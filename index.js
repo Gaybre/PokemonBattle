25c767bbac5de6b0fa488ac704b6a97c1d71da32
JuanGaybre <jugaybre@gmail.com>
2020-01-24 17:38:12 -0600

    Ordenando la entrada de los datos a las funciones correspondientes

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const $playerName1 		= document.getElementById('namePlayer1')
 const $playerName2 		= document.getElementById('namePlayer2')
 const $chosePokemon 	= document.getElementById('container-choose-pokemon')
 var namePlayer1, namePlayer2
+var n = 1
 
 $start.addEventListener('click', empezar)
 $btnPlayer.addEventListener('click', playerName)
@@ -22,34 +23,43 @@ function showInstructions() {
 		$instructions.classList.toggle('start')
 	}, 2000)
 }
-showInstructions()
-
 function empezar(){
-	$turno.classList.toggle('Turno')
 	$instructions.classList.toggle('start')
+	$turno.classList.toggle('Turno')
 	$chosePlayerName.classList.toggle('choosePlayerName')
-	$p1.classList.toggle('indicator')
 	$footer.classList.add('down')
+	setTimeout(() => $p1.classList.toggle('indicator'), 500)
 }
-var n = 1
 function playerName() {
 	switch(n) {
 		case 1:
-			namePlayer1 = $txtPlayer.value
-			$playerName1.innerHTML = $txtPlayer.value
-			$txtPlayer.value = ''
-			n++
-			$p1.classList.toggle('indicator')
-			$p2.classList.toggle('indicator')
-			break
+			if ($txtPlayer.value == '') {
+				alert(`Player ${n}, debes ingresar un nombre de usuario`)
+			}else{
+				namePlayer1 = $txtPlayer.value
+				$playerName1.innerHTML = namePlayer1
+				$txtPlayer.value = ''
+				n++
+				$p1.classList.toggle('indicator')
+				$p2.classList.toggle('indicator')
+			}
+		break
 		case 2:
-			namePlayer2 = $txtPlayer.value
-			$playerName2.innerHTML = $txtPlayer.value
-			n++
-		case 3:
-			$p2.classList.toggle('indicator')
-			$chosePlayerName.classList.toggle('choosePlayerName')
-			$chosePokemon.classList.toggle('containerChoosePokemon')
-			setTimeout(() => $p1.classList.toggle('indicator'), 500)
+			if ($txtPlayer.value == '') {
+				alert(`Player ${n}, debes ingresar un nombre de usuario`)
+			}else{
+				namePlayer2 = $txtPlayer.value
+				$playerName2.innerHTML = namePlayer2
+				n--
+				setTimeout(() => {
+					$p2.classList.toggle('indicator')
+					$chosePlayerName.classList.toggle('choosePlayerName')
+					$chosePokemon.classList.toggle('containerChoosePokemon')
+					setTimeout(() => $p1.classList.toggle('indicator'), 500)
+					ShowPokemonAvailable()
+				}, 400)
+			}
+		break
 	}
 }
+showInstructions()
--- pokemon.js
+++ pokemon.js
@@ -28,4 +28,3 @@ async function ShowPokemonAvailable() {
 		}
 	}
 }
-ShowPokemonAvailable()
